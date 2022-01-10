ceb7214786e4884290963cce3f3638800546cc7b
Pravin Jadhav <pr.jadhav02@gmail.com>
2022-01-10 09:35:47 +0530

    update

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <p>WELCOME TO WEBSITE</p>
+            <h1>We are the coder</h1>
+        </div>
+    )
+}
+
+export default Home
